e4779b848b70b62373f635787fe35128123e4468
Sujanth <sujanth1987@gmail.com>
2017-05-01 14:01:56 +1000

    added new category

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -16,6 +16,31 @@ export default [
         id: '3',
         person: 'Robert H. Schuller',
         text: 'What great thing would you attempt if you knew you could not fail?'
+      },
+      {
+        id: '4',
+        person: 'Vincent Van Gogh',
+        text: 'If you hear a voice within you say ‘you cannot paint,’ then by all means paint, and that voice will be silenced.'
+      },
+      {
+        id: '5',
+        person: 'Mark Twain',
+        text: 'Don’t go around saying the world owes you a living. The world owes you nothing. It was here first.'
+      },
+      {
+        id: '6',
+        person: 'Winston Churchill',
+        text: 'Success consists of going from failure to failure without loss of enthusiasm.'
+      },
+      {
+        id: '7',
+        person: 'Alexander Graham Bell',
+        text: 'When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.'
+      },
+      {
+        id: '8',
+        person: 'Thomas A. Edison',
+        text: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'
       }
     ],
     icon: 'brush'
@@ -24,14 +49,34 @@ export default [
     category: 'ability',
     quotes: [
       {
-        id: '4',
+        id: '9',
         person: 'John Wooden',
         text: 'Ability may get you to the top, but it takes character to keep you there.'
       },
       {
-        id: '5',
+        id: '10',
         person: 'Robert Frost',
         text: 'Education is the ability to listen to almost anything without losing your temper.'
+      },
+      {
+        id: '11',
+        person: 'William James',
+        text: 'The greatest weapon against stress is our ability to choose one thought over another'
+      },
+      {
+        id: '12',
+        person: 'Charles B. Rangel',
+        text: 'A quality education grants us the ability to fight the war on ignorance and poverty'
+      },
+      {
+        id: '13',
+        person: 'Winston Churchill',
+        text: 'My most brilliant achievement was my ability to be able to persuade my wife to marry me.'
+      },
+      {
+        id: '14',
+        person: 'Bob Marley',
+        text: 'The greatness of a man is not in how much wealth he acquires, but in his integrity and his ability to affect those around him positively.'
       }
     ],
     icon: 'bicycle'
@@ -40,14 +85,34 @@ export default [
     category: 'enthusiasm',
     quotes: [
       {
-        id: '6',
+        id: '15',
         person: 'Benjamin Disraeli',
         text: 'Every product of genius must be the product of enthusiasm.'
       },
       {
-        id: '7',
+        id: '16',
         person: 'Norman Vincent Peale',
         text: 'Enthusiasm releases the drive to carry you over obstacles and adds significance to all you do.'
+      },
+      {
+        id: '17',
+        person: 'Bo Bennett',
+        text: 'Enthusiasm is excitement with inspiration, motivation, and a pinch of creativity.'
+      },
+      {
+        id: '18',
+        person: 'Adam Smith',
+        text: 'Science is the great antidote to the poison of enthusiasm and superstition.'
+      },
+      {
+        id: '19',
+        person: 'Dale Carnegie',
+        text: 'Flaming enthusiasm, backed up by horse sense and persistence, is the quality that most frequently makes for success.'
+      },
+      {
+        id: '20',
+        person: 'Pele',
+        text: 'Enthusiasm is everything. It must be taut and vibrating like a guitar string.'
       }
     ],
     icon: 'battery-charging'
@@ -56,16 +121,37 @@ export default [
     category: 'motivational',
     quotes: [
       {
-        id: '8',
+        id: '21',
         person: 'Jim Rohn',
         text: 'Either you run the day or the day runs you.'
       },
       {
-        id: '9',
+        id: '22',
         person: 'Donna Brazile',
         text: 'I was motivated to be different in part because I was different.'
       }
     ],
     icon: 'body'
+  },
+  {
+    category: 'programming',
+    quotes: [
+      {
+        id: '23',
+        person: 'Jim McCarthy',
+        text: 'You can’t have great software without a great team, and most software teams behave like dysfunctional families.'
+      },
+      {
+        id: '24',
+        person: 'Martin Golding',
+        text: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.'
+      },
+      {
+        id: '25',
+        person: 'C.A.R. Hoare',
+        text: 'There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies. And the other way is to make it so complicated that there are no obvious deficiencies.'
+      }
+    ],
+    icon: 'code'
   }
 ];

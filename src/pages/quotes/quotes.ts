3695743c2961c315bff5583972f9508284ea8c24
Sujanth <sujanth1987@gmail.com>
2017-05-01 15:26:02 +1000

    Created new service to add, remove and get quotes

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 
 import { Quote } from '../../data/quote.interface';
 
@@ -12,7 +12,8 @@ export class Quotes implements OnInit{
   quoteGroup: { category: String, quotes: Quote[], icon: String};
 
   constructor (
-    private navParams: NavParams
+    private navParams: NavParams,
+    private alertCtrl: AlertController
   ) {}
 
   // ionViewDidLoad() {
@@ -22,6 +23,31 @@ export class Quotes implements OnInit{
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(SelectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Pressed YES button');
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Pressed Cancel button');
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
